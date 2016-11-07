4c771f10a58f60717e1d6477998ac0a474911b18
allenYetu211 <allenwill211@gmail.com>
2016-11-07 17:06:42 +0800

    start

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+  <navigation></navigation>
+  </div>
+</template>
+
+<script>
+import Navigation from './components/navigation'
+export default {
+  components: {
+    Navigation
+  }
+}
+</script>
+
+<style>
+
+</style>
